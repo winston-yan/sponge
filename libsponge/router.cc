131c18d3c4744dd076b6e9d7fe5706281b1742c5
root <root@DESKTOP-5J53U1H.localdomain>
2021-12-31 16:00:33 +0800

    FIN lab6: format router.cc

--- libsponge/router.cc
+++ libsponge/router.cc
@@ -15,20 +15,18 @@ void Router::add_route(const uint32_t route_prefix,
                        const std::optional<Address> next_hop,
                        const size_t interface_num) {
     std::cerr << "DEBUG: adding route " << Address::from_ipv4_numeric(route_prefix).ip() << "/" << int(prefix_length)
-         << " => " << (next_hop.has_value() ? next_hop->ip() : "(direct)") << " on interface " << interface_num << std::endl;
+              << " => " << (next_hop.has_value() ? next_hop->ip() : "(direct)") << " on interface " << interface_num
+              << std::endl;
 
-    _table.push_back(RouteEntry{route_prefix,
-                                prefix_length,
-                                next_hop,
-                                interface_num
-                               });
-    return ;
+    _table.push_back(RouteEntry{route_prefix, prefix_length, next_hop, interface_num});
+    return;
 }
 
 //! \param[in] dgram The datagram to be routed
 void Router::route_one_datagram(InternetDatagram &dgram) {
     /* decrement TTL, DROP dgram if TTL equal to zero */
-    if (dgram.header().ttl == 0 || --dgram.header().ttl == 0) return ;
+    if (dgram.header().ttl == 0 || --dgram.header().ttl == 0)
+        return;
 
     size_t max_len = 0;
     uint32_t dst_ip_address = dgram.header().dst;
@@ -36,9 +34,9 @@ void Router::route_one_datagram(InternetDatagram &dgram) {
     int match_index = 0, n = _table.size();
     for (int i = 0; i < n; ++i) {
         RouteEntry &e = _table[i];
-        if (e.prefix_length < max_len) continue;
-        uint32_t res = dst_ip_address ^ e.route_prefix;
-        if (e.prefix_length == 0 || (res >> (32 - e.prefix_length)) == 0) {
+        if (e.prefix_length < max_len)
+            continue;
+        if (e.prefix_length == 0 || ((dst_ip_address ^ e.route_prefix) >> (32 - e.prefix_length)) == 0) {
             found_match = 1;
             max_len = e.prefix_length;
             match_index = i;
@@ -46,13 +44,15 @@ void Router::route_one_datagram(InternetDatagram &dgram) {
     }
 
     /* DROP dgram: no entry match the destination ip address */
-    if (!found_match) return ;
+    if (!found_match)
+        return;
 
     const RouteEntry &e = _table[match_index];
-    interface(e.interface_num).send_datagram(dgram,
-            e.next_hop.has_value() ? e.next_hop.value() : Address::from_ipv4_numeric(dgram.header().dst));
+    interface(e.interface_num)
+        .send_datagram(dgram,
+                       e.next_hop.has_value() ? e.next_hop.value() : Address::from_ipv4_numeric(dgram.header().dst));
 
-    return ;
+    return;
 }
 
 void Router::route() {
